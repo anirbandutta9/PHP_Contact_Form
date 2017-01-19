a0caf357f37f5e79bd2ff022aa13602addbdba12
Anirban Dutta <anirbandutta011@gmail.com>
2017-01-19 18:46:02 +0530

    Update thank.php

--- thank.php
+++ thank.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
+if(isset($_SESSION['issubmit'])) {
+echo "Thank u " .$_SESSION['NAME'] . " " . "for registering with us <br/> ";
+echo "We have sent a msg to " .  $_SESSION['EMAIL'] . " " . "for ur conviniance";
+session_destroy();
+}
+else {
+	header("Location: index.php");
+}
 ?>
-<?php echo "Thank u " .$_SESSION['NAME'] . " " . "for registering with us " ?>
-<?php echo "We have sent a msg to " .  $_SESSION['EMAIL'] . " " . "for ur conviniance" ?>
-
